a6f74d82634b7efbe09742ed60604c1a681eb9bc
mbledkowski <pub@mble.dk>
2024-11-01 14:48:18 +0100

    Removed the scalachess experiment

--- backend/scala/project/Dependencies.scala
+++
@@ -1,9 +0,0 @@
-import sbt._
-
-object Dependencies {
-  lazy val lilaMaven = "lila-maven" at "https://raw.githubusercontent.com/lichess-org/lila-maven/master"
-  lazy val munit = "org.scalameta" %% "munit" % "0.7.29"
-  lazy val chessVersion = "16.3.2"
-  lazy val chess = "org.lichess" %% "scalachess" % chessVersion
-  lazy val chessTestKit  = "org.lichess" %% "scalachess-test-kit" % chessVersion
-}
