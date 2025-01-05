1c5046c52942cd1462268c00acbcda0a56b65781
mbledkowski <pub@mble.dk>
2025-01-05 18:20:55 +0100

    Start writing movement logic

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,22 +1,48 @@
 import assert from 'node:assert/strict';
 
 type type = "pawn" | "rook" | "knight" | "bishop" | "queen" | "king" | "unicorn";
+type shortType = "p" | "r" | "n" | "b" | "q" | "k" | "u";
 type color = "white" | "black";
 
-class Piece {
-  constructor(public type: type, public color: color, public x: number, public y: number, public z: number) {
-    this.type = type;
-    this.color = color;
-    this.x = x;
-    this.y = y;
-    this.z = z;
+class Position {
+  constructor(public x: number, public y: number, public z: number) {}
+}
+
+class Piece extends Position {
+  private typeToShortType: Record<type, shortType> = {
+    "pawn": "p",
+    "rook": "r",
+    "knight": "n",
+    "bishop": "b",
+    "queen": "q",
+    "king": "k",
+    "unicorn": "u"
   }
+  private shortTypeToType: Record<shortType, type> = {
+    "p": "pawn",
+    "r": "rook",
+    "n": "knight",
+    "b": "bishop",
+    "q": "queen",
+    "k": "king",
+    "u": "unicorn"
+  }
+  public shortType: shortType;
+
+  constructor(public type: type, public color: color, x: number, y: number, z: number) {
+    super(x, y, z);
+    this.shortType = this.typeToShortType[this.type]
+  }
+}
+
+class Move {
+  constructor(public from: Position, public to: Position) {}
 }
 
 export class Board {
   pieces: Piece[];
   player: color;
-  squares: (Piece | null)[][][] = Array.from({length:5}, () => Array.from({length:5}, () => new Array(5).fill(null)));
+  public squares: (Piece | null)[][][] = Array.from({length:5}, () => Array.from({length:5}, () => new Array(5).fill(null)));
   notation: string;
 
   updateSquares() {
@@ -80,7 +106,7 @@ export class Board {
     this.notation += `#${this.player === "white" ? "w" : "b"}#`
   }
 
-  parseNotation(notation: string): [Piece[], color] {
+  private parseNotation(notation: string): [Piece[], color] {
     const boardStart = notation.indexOf("/");
     const boardEnd = notation.lastIndexOf("/");
     const statusStart = notation.indexOf("#");
@@ -186,6 +212,95 @@ export class Board {
     this.notation = startNotation;
     [this.pieces, this.player] = this.parseNotation(startNotation);
     this.updateSquares();
+  }
+
+  private generateMovesInDirection(pos: Position, xDir: number, yDir: number, zDir: number) {
+    const moves: Move[] = [];
+    const currentPos = new Position(pos.x + xDir, pos.y + yDir, pos.z + zDir);
+    while (currentPos.x > 0 && currentPos.x <= 5 &&
+           currentPos.y > 0 && currentPos.y <= 5 &&
+           currentPos.z > 0 && currentPos.z <= 5) {
+      const currentSquare = this.squares[currentPos.x-1][currentPos.y-1][currentPos.z-1];
+      if (currentSquare !== null) {
+        if (currentSquare.color !== this.player) {
+          moves.push(new Move(pos, currentPos));
+        }
+        break;
+      }
+      moves.push(new Move(pos, currentPos));
+    }
+    return moves;
+  }
+
+  private generateSlidingMoves(pos: Position, type: type) {
+    const moves: Move[] = [];
+    switch(type) {
+      case "queen":
+        for (let xDir = -1; xDir <= 1; xDir++){
+          for (let yDir = -1; yDir <= 1; yDir++){
+            for (let zDir = -1; zDir <= 1; zDir++){
+              if (xDir === 0 && yDir === 0 && zDir === 0) {
+                continue; // Cannot move into itself
+              }
+              moves.push(...this.generateMovesInDirection(pos, xDir, yDir, zDir));
+            }
+          }
+        }
+        break;
+      case "rook":
+        for (let dim = 0; dim < 3; dim++) {
+          for (let dir = -1; dir <= 1; dir += 2) {
+            const xDir = dim === 0 ? dir : 0;
+            const yDir = dim === 1 ? dir : 0;
+            const zDir = dim === 2 ? dir : 0;
+            moves.push(...this.generateMovesInDirection(pos, xDir, yDir, zDir));
+          }
+        }
+        break;
+      case "bishop":
+        for (let dir = -1; dir <= 1; dir += 2) {
+          for (let dir2 = -1; dir2 <= 1; dir2 += 2) {
+            moves.push(...this.generateMovesInDirection(pos, 0, dir, dir2));
+            moves.push(...this.generateMovesInDirection(pos, dir, 0, dir2));
+            moves.push(...this.generateMovesInDirection(pos, dir, dir2, 0));
+          }
+        }
+        break;
+      case "unicorn":
+        for (let xDir = -1; xDir <= 1; xDir += 2){
+          for (let yDir = -1; yDir <= 1; yDir += 2){
+            for (let zDir = -1; zDir <= 1; zDir += 2){
+              moves.push(...this.generateMovesInDirection(pos, xDir, yDir, zDir));
+            }
+          }
+        }
+        break;
+    }
+    return moves;
+  }
+
+  public possibleMovesPseudoLegal(): Move[] {
+    const moves: Move[] = [];
+    for (const piece of this.pieces) {
+      if (piece.color !== this.player) {
+        continue;
+      }
+      switch (piece.type) {
+        case "pawn":
+          break;
+        case "knight":
+          break;
+        case "king":
+          break;
+        default:
+          moves.push(...this.generateSlidingMoves(new Position(piece.x, piece.y, piece.z), piece.type));
+      }
+    }
+    return moves;
+  }
+  public possibleMovesLegal(): Move[] {
+    //TODO
+    return this.possibleMovesPseudoLegal();
   }
 }
 
