1513b24da682f5b3c8ef0f65a049d6e6b8fa9c6c
mbledkowski <pub@mble.dk>
2025-01-16 21:30:39 +0100

    Update cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    allowedHeaders: ['content-type'],
+    origin: 'http://localhost:4200/',
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 
